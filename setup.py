a895a2e54b01840b64d673155e2d1f98f978ec33
io <gie9ohbeixah@paperboats.net>
2021-05-27 23:08:57 +0000

    [wip] copy more of the source code to the sdist

--- setup.py
+++ setup.py
@@ -1,35 +1,19 @@
 #!/usr/bin/env python3
 
-import platform
-from setuptools import setup, Extension
-
-
-class PyBind11Include:
-    def __str__(self):
-        import pybind11
-
-        return pybind11.get_include()
-
-
-extra_compile_args = []
-if platform.system() == "Windows":
-    extra_compile_args.append('/std:c++17')
-else:
-    extra_compile_args.append('-std=c++17')
-
+import glob
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
 
 setup(
     ext_modules=[
-        Extension(
+        Pybind11Extension(
             'pytomlpp._impl',
-            ['src/pytomlpp.cpp', 'src/type_casters.cpp', 'src/encoding_decoding.cpp'],
+            ['src/pytomlpp.cpp', 'src/type_casters.cpp', 'src/encoding_decoding.cpp', *glob.glob('third_party/**/*.cpp', recursive=True), *glob.glob('external/**/*.cpp', recursive=True)],
             include_dirs=[
                 'include',
                 'third_party',
-                PyBind11Include(),
             ],
-            extra_compile_args=extra_compile_args,
-            language='c++',
+			cxx_std=17,
         ),
     ],
 )
